741af380ca14788bb83e5dd5a7825dd2fdf1759b
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2022-11-18 12:39:40 -0700

    fixed merge conflict

--- main.py
+++ main.py
@@ -3,11 +3,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-<<<<<<< Updated upstream
-from scipy.optimize import Bounds, minimize
-
-=======
->>>>>>> Stashed changes
 from rk4 import rk4
 from train_motion import train_motion
 from optimize_method import optimize, local_optimize
@@ -18,150 +13,6 @@ from optimize_method import optimize, local_optimize
 #  - Run optimization again
 
 
-<<<<<<< Updated upstream
-class Res(object):
-    def __init__(self, params, time):
-        # store params into dictionary x
-        self.x = {"Lt": params[0], "Rt": params[1], "P0": params[2], "Rg": params[3], "Ls": params[4], "Rp": params[5],
-                  "dens": params[6]}
-        self.time = time
-
-
-def run_race_simulation(params):
-    try:
-        h = 0.01
-        tspan = np.arange(0.0, 10, h)
-        t, y = rk4(train_motion, tspan, y0, h, params)
-    except Slipped:  # if train slips, return a large time
-        return 101
-    if max(y[:, 0]) < 10:  # if train doesn't reach the finish line, return a large time
-        return 99
-    else:
-        for index, position in enumerate(y[:, 0]):
-            if position >= 10:
-                if max(y[:, 0]) > 12.5:  # if train goes too far, return a large time
-                    return 105
-                return t[index]
-
-
-def random_param(bounds):
-    return np.random.uniform(bounds[0], bounds[1])
-
-
-def create_bounds_in_range(var, dist):
-    var_s = dist * var
-    bounds = (var - var_s, var + var_s)
-    return bounds
-
-
-def optimize(params_bounds, num_trials):
-    """ what hyrum started
-    def optimize(params_bounds, num_trials):
-        best_params = None
-        best_time = None
-        for trial in range(num_trials):
-            # Randomize Parameters
-    """
-    # Create Bounds For Scipi minimize
-    #               Lower Bounds                                Upper Bounds
-    #               Lt    Rt    P0     Rg     Ls   Rp    dens   Lt    Rt   P0     Rg    Ls   Rp    dens
-    bounds = Bounds([0.2, 0.05, 70000, 0.002, 0.1, 0.02, 1200], [0.3, 0.2, 200000, 0.01, 0.5, 0.04, 8940])
-
-    time_list = []
-    params_list = []
-    best_params = None
-    best_time = None
-    for trial in range(num_trials):
-        # Ramdomize Parameters
-        Lt = random_param(params_bounds["Lt"])
-        Rt = random_param(params_bounds["Rt"])
-        P0 = random_param(params_bounds["P0"])
-        Rg = random_param(params_bounds["Rg"])
-        Ls = random_param(params_bounds["Ls"])
-        Rp = random_param(params_bounds["Rp"])
-        dens = random_param(params_bounds["dens"])
-
-        params = np.array([Lt, Rt, P0, Rg, Ls, Rp, dens])
-        raceTime = run_race_simulation(params)
-
-        # if this is the first trial, set best_params and best_time
-        if best_params is None:
-            best_params = params
-            best_time = raceTime
-
-        # if this is not the first trial, compare to best_params and best_time
-        if raceTime < best_time:
-            best_params = params
-            best_time = raceTime
-            print(f"Best parameters so far:")
-            print(f"\tLt: {best_params[0]}")
-            print(f"\tRt: {best_params[1]}")
-            print(f"\tP0: {best_params[2]}")
-            print(f"\tRg: {best_params[3]}")
-            print(f"\tLs: {best_params[4]}")
-            print(f"\tRp: {best_params[5]}")
-            print(f"\tdensity: {best_params[6]}")
-            print(f"Optimized cost (time): {best_time}")
-            print(f"Copyable: {best_params.tolist()}\n")
-
-    else:
-        res = Res(best_params, best_time)
-
-    return res
-
-
-def local_optimize(params, num_trials, dist):
-    # Create local bounds for each parameter
-    Lt_bounds = create_bounds_in_range(params[0], dist)
-    Rt_bounds = create_bounds_in_range(params[1], dist)
-    P0_bounds = create_bounds_in_range(params[2], dist)
-    Rg_bounds = create_bounds_in_range(params[3], dist)
-    Ls_bounds = create_bounds_in_range(params[4], dist)
-    Rp_bounds = create_bounds_in_range(params[5], dist)
-    density_bounds = create_bounds_in_range(params[6], dist)
-
-    best_params = None
-    best_time = None
-
-    for trial in range(num_trials):
-        Lt = random_param(Lt_bounds)
-        Rt = random_param(Rt_bounds)
-        P0 = random_param(P0_bounds)
-        Rg = random_param(Rg_bounds)
-        Ls = random_param(Ls_bounds)
-        Rp = random_param(Rp_bounds)
-        dens = random_param(density_bounds)
-
-        params = np.array([Lt, Rt, P0, Rg, Ls, Rp, dens])
-        raceTime = run_race_simulation(params)
-
-        # if this is the first trial, set best_params and best_time
-        if best_params is None:
-            best_params = params
-            best_time = raceTime
-
-        # if this is not the first trial, compare to best_params and best_time
-        if raceTime < best_time:
-            best_params = params
-            best_time = raceTime
-            print(f"Best parameters so far:")
-            print(f"\tLt: {best_params[0]}")
-            print(f"\tRt: {best_params[1]}")
-            print(f"\tP0: {best_params[2]}")
-            print(f"\tRg: {best_params[3]}")
-            print(f"\tLs: {best_params[4]}")
-            print(f"\tRp: {best_params[5]}")
-            print(f"\tdensity: {best_params[6]}")
-            print(f"Optimized cost (time): {best_time}")
-            print(f"Copyable: {best_params.tolist()}\n")
-
-    else:
-        res = Res(best_params, best_time)
-    return res
-
-
-=======
->>>>>>> Stashed changes
 def main():
     # Initialize bounds
     Lt_bounds = (0.2, 0.3)
@@ -186,11 +37,7 @@ def main():
 
     res = optimize(params_bounds, num_trials)
     print("Completed coarse optimization, beginning local optimization")
-<<<<<<< Updated upstream
-    res = local_optimize(res.x, num_trials, 0.01)
-=======
-    res = local_optimize(res.x, num_trials, 0.1)
->>>>>>> Stashed changes
+    res = local_optimize(res, num_trials, .01)
     print("Local optimization complete.")
 
     print(f"Final parameters:")
